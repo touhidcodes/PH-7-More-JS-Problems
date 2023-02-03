ae2e79afd5cc829671a1424c46625d06c6d30f9e
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-03 19:53:04 +0600

    cart

--- cart.js
+++ cart.js
@@ -2,22 +2,27 @@ const shoppingCart = [
 	{
 		name: "shoe",
 		price: 1200,
+		quantity: 2,
 	},
 	{
 		name: "shirt ",
 		price: 1000,
+		quantity: 5,
 	},
 	{
 		name: "pant",
 		price: 1500,
+		quantity: 2,
 	},
 	{
 		name: "belt",
 		price: 600,
+		quantity: 2,
 	},
 	{
 		name: "sunglass",
 		price: 1000,
+		quantity: 2,
 	},
 ];
 
@@ -25,7 +30,7 @@ function sumShopping(product) {
 	let total = 0;
 	for (let i = 0; i < product.length; i++) {
 		const element = product[i];
-		const price = element.price;
+		const price = element.price * element.quantity;
 		total += price;
 	}
 	return total;
